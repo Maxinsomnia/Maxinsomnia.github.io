872800d39a44e2f6929140da37b6b5ec79b3529e
Maxinsomnia <insomniaguo@163.com>
2022-06-11 22:29:36 +0800

    :pencil: update content

---
+++ source/_posts/test.md
@@ -0,0 +1,10 @@
+---
+title: test
+date: 2022-06-11 22:22:47
+tags: #kk #see
+categories：#ok
+---
+
+# I just want
+
+want to see how it works like
---
+++ update.sh
@@ -0,0 +1,9 @@
+hexo clean
+hexo g
+info=$1
+if ["$info" = ""];
+then info=":pencil: update content"
+fi
+git add -A
+git commit -m "$info"
+git push origin hexo
