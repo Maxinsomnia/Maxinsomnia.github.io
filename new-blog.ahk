78943d6eb24c6b687bd18c761cff071288839d55
Maxinsomnia <insomniaguo@163.com>
2022-06-17 01:42:14 +0800

    default: update content

--- new-blog.ahk
+++ new-blog.ahk
@@ -1,3 +1,3 @@
 #!N::
-run, "G:\blog\maxinsomnia.github.io\new-blog.ps1",,hide
+run, "C:\Users\Guo\blog\maxinsomnia.github.io\new-blog.ps1",,hide
 return
--- new-blog.ps1
+++ new-blog.ps1
@@ -1,11 +1,11 @@
 ## 计数作为文件名
-$count = [System.IO.Directory]::GetFiles("G:\blog\maxinsomnia.github.io\source\_posts").Count
+$count = [System.IO.Directory]::GetFiles("C:\Users\Guo\blog\maxinsomnia.github.io\source\_posts").Count
 ## 提前写入 Markdown 的 yaml 头
 $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 $yaml = "---" + "`ntitle: $count" + "`ndate: '$date'" + "`nupdated: '$date'" + "`ncategories: []" + "`ntags: [blog]" + "`ndescription:" +"`n`n---"
 ## 在 Hexo 中创建文件
-Write-Output $yaml | Out-File G:\blog\maxinsomnia.github.io\source\_posts\$count.md
+Write-Output $yaml | Out-File C:\Users\Guo\blog\maxinsomnia.github.io\source\_posts\$count.md
 ## 在 Obsidian 中创建硬链接
-New-Item C:\Users\Guo\iCloudDrive\iCloud~md~obsidian\blog\$count.md -Type HardLink -Target G:\blog\maxinsomnia.github.io\source\_posts\$count.md
+New-Item C:\Users\Guo\iCloudDrive\iCloud~md~obsidian\blog\$count.md -Type HardLink -Target C:\Users\Guo\blog\maxinsomnia.github.io\source\_posts\$count.md
 ## 退出
 exit
